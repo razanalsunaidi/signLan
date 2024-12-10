0131eb88e44ead71393a9205c0725a6927aad2f6
razanalsunaidi <razanalsunedi@gmail.com>
2024-12-10 14:53:55 +0300

    Initial Commit

---
+++ signLan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  signLan
+//
+//  Created by Razan Alsunaidi on 09/06/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ signLan/signLanApp.swift
@@ -0,0 +1,17 @@
+//
+//  signLanApp.swift
+//  signLan
+//
+//  Created by Razan Alsunaidi on 09/06/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct signLanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
